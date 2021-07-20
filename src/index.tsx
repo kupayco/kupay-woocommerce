3ad7d2302824d0ac330afc5297c0fa85a7c3ec39
Damian <sanchezdamian6@gmail.com>
2021-07-20 11:18:11 +0200

    fixing

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { MaterialUiThemeProvider } from "./contexts/index";
+import theme from "./styles/theme";
+import { ThemeProvider } from "@material-ui/styles";
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
