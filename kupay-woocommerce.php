3b4c2536cbbcf8b18d7f42670f9b7ede4a3003f9
Jaf Gonçalves <jaf@kupay.co>
2021-10-29 12:25:13 +0200

    Adjust button position for PDP

--- kupay-woocommerce.php
+++ kupay-woocommerce.php
@@ -29,7 +29,7 @@ add_action( 'wp_enqueue_scripts', 'kupay_enqueue_kupay_css' );
 add_action('admin_menu', 'kupay_create_settings_menu');
 
 // Templates
-add_action( 'woocommerce_before_add_to_cart_form', 'kupay_render_pdp', 20);
+add_action( 'woocommerce_before_add_to_cart_button', 'kupay_render_pdp', 20);
 add_action( 'woocommerce_proceed_to_checkout', 'kupay_render_cart', 10 );
 add_action( 'woocommerce_before_checkout_form', 'kupay_render_checkout', 10 );
 
