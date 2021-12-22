d1b52692bc43cede46cfb88d4b578fd10b216989
Nicolas Sciuto <nicolas@novolabs.xyz>
2021-12-22 08:49:36 -0300

    add confirmation box and fix error msgs

--- assets/refund.js
+++ assets/refund.js
@@ -3,35 +3,40 @@ function kupayRefundOrder() {
     let appId = document.getElementById('kupay-app-id');
     let url = document.getElementById('kupay-api-url');
 
-    if (order) {
-        url = url.value + '/order/refund';
-        appId = appId.value;
-        order = JSON.parse(order.value);
-        const storeOrderId = order.id;
-        const orderId = getKupayOrderId(order.meta_data);
+    if (order && appId && url) {
+        const confirmation = window.confirm(
+            `You are about to refund this order. \nDo you wish to proceed ?`
+        );
+        if(confirmation) {
+            url = url.value + '/order/refund';
+            appId = appId.value;
+            order = JSON.parse(order.value);
+            const storeOrderId = order.id;
+            const orderId = getKupayOrderId(order.meta_data);
 
-        const data = {
-            appId: String(appId),
-            storeOrderId: String(storeOrderId),
-            orderId: String(orderId),
+            const data = {
+                appId: String(appId),
+                storeOrderId: String(storeOrderId),
+                orderId: String(orderId),
+            }
+
+            fetch(url, {
+                method: 'PUT',
+                mode: 'cors',
+                cache: 'no-cache',
+                credentials: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                redirect: 'follow',
+                referrerPolicy: 'no-referrer',
+                body: JSON.stringify(data)
+            }).then(response => {
+                return response.text();
+            }).then(response => {
+                alert(response);
+            }).catch(err => alert(err.message));
         }
-
-        fetch(url, {
-            method: 'PUT',
-            mode: 'cors',
-            cache: 'no-cache',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            redirect: 'follow',
-            referrerPolicy: 'no-referrer',
-            body: JSON.stringify(data)
-        }).then(response => {
-            return response.text();
-        }).then(response => {
-            alert(response);
-        }).catch(err => alert(err.message));
 
     } else {
         alert('Sorry, there has been an error');
